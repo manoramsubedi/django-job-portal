b36cf5bf72307418775cda368214b589adf38444
Manoram <manoramsubedi7@gmail.com>
2024-11-21 19:14:32 +0545

    initial

---
+++ candidate/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+from hr.models import CandidateApplication, JobPost
+# Create your models here.
+
+
+class MyJobList(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    job = models.ForeignKey(CandidateApplication, on_delete=models.CASCADE)
+    applied_date = models.DateField(auto_now_add=True)
+
+class IsSortList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    job = models.OneToOneField(JobPost, on_delete=models.CASCADE)
+    applied_date = models.DateField(auto_now_add=True)
---
+++ hr/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class CandidateApplication(models.Model):
+    pass
+
+class JobPost(models.Model):
+    pass

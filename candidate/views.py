7c5794fc234c85cf75830db7078484d1ef5d61bd
Manoram <manoramsubedi7@gmail.com>
2024-11-30 11:12:23 +0545

    job apply

--- candidate/views.py
+++ candidate/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-from hr.models import JobPost
+from .models import MyJobList
+from hr.models import JobPost, CandidateApplication
 
 # Create your views here.
 
@@ -19,5 +20,22 @@ def joblist(request):
 
 @login_required
 def applyforjob(request, pk):
-    print(pk)
+    if JobPost.objects.filter(id=pk).exists():
+        job = JobPost.objects.get(id=pk)
+        if CandidateApplication.objects.filter(user=request.user, job=job).exists():
+            return redirect('candidate_dashboard')
+
+        if request.method == 'POST':
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            college = request.POST.get('college')
+            passing_year = request.POST.get('passing_year')
+            yearOfExperience =request.POST.get('yearOfExperience')
+            resume = request.FILES.get('resume')
+
+            candidate_applicaction = CandidateApplication(user=request.user, job=job, pass_year=passing_year, experience=yearOfExperience, resume=resume)
+            candidate_applicaction.save()
+            MyJobList(user=request.user, job=candidate_applicaction).save()
+            return redirect('candidate_dashboard')
+
     return render(request, 'candidate/jobapply.html')
